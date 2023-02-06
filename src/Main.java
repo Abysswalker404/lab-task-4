d2edf85f72ecb61f4f946b4a5930179360036c1c
Abysswalker404 <84619179+Abysswalker404@users.noreply.github.com>
2023-02-07 00:43:52 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double r =5, result;
+        result = 3.1416*r*r;
+        System.out.println("Area of circle:"+result);
+    }
+}
